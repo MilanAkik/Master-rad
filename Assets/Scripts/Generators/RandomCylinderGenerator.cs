7da7fb8358a330357b5aa4d780c4730eac27a815
MilanAkik <mixa.mixony@gmail.com>
2025-10-11 23:56:50 +0200

    Added a cylinder generator that uses the random generator for all the components

---
+++ Assets/Scripts/Generators/RandomCylinderGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Assets.Scripts.Generators
+{
+    public class RandomCylinderGenerator : CylinderGenerator
+    {
+        public override Vector4[] getCylinders(params object[] objects)
+        {
+            int count = (int)objects[0];
+            int seed = (int)objects[1];
+            var prevState = Random.state;
+            Random.InitState(seed);
+            Vector4[] res = new Vector4[count];
+            for (int i = 0; i < count; i++)
+            {
+                var x = Random.Range(-1.0f, 1.0f);
+                var y = Random.Range(-0.5f, 0.5f);
+                var r = Random.Range(0.01f, 0.99f);
+                var z = Random.Range(-1.0f, 1.0f);
+                res[i] = new Vector4(x, y, z, r);
+            }
+            Random.state = prevState;
+            return res;
+        }
+    }
+}
