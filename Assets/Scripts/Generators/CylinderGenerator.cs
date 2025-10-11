4231ffde6d26b4603042c71cb38a29a4bc0f21ef
MilanAkik <mixa.mixony@gmail.com>
2025-10-11 23:13:12 +0200

    Added an abstract class to represent the generator of the cylinders

---
+++ Assets/Scripts/Generators/CylinderGenerator.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Generators
+{
+    public abstract class CylinderGenerator
+    {
+        public abstract Vector4[] getCylinders(params object[] objects);
+    }
+}
