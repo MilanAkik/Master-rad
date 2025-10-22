123f8fdb500b4fee853298f202e811b1e07ff4bc
MilanAkik <mixa.mixony@gmail.com>
2025-10-23 00:10:44 +0200

    Cleanup

--- Assets/Scripts/CylinderRenderer.cs
+++ Assets/Scripts/CylinderRenderer.cs
@@ -20,15 +20,7 @@ public class CylinderRenderer : MonoBehaviour
             _cylinders[1] = new Vector4(-2, -0.5f, 20, 0.5f);
             _cylinders[2] = new Vector4( 2, 1.5f, 15, 0.75f);
             Camera cam = Camera.current ?? Camera.main;
-            //Debug.LogError(cam.transform.position);
-            //Debug.LogError(cam.cameraToWorldMatrix);
-            //Debug.LogError(cam.projectionMatrix.inverse);
-            //var res = cam.projectionMatrix * new Vector4(-1, -1, -1, 1);
-            //res = res / res.w;
-            //var n = new Vector4(res.x, res.y, res.z, 0);
-            //var o = cam.cameraToWorldMatrix * n;
-            //Debug.LogError(o.normalized);
-
+            
             // Camera parameters
             raymarchMat.SetMatrix("_CamToWorld", cam.cameraToWorldMatrix);
             raymarchMat.SetMatrix("_CamInverseProjection", cam.projectionMatrix.inverse);
