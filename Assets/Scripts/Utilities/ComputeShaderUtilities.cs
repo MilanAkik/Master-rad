441b647cbee38eb5eb6e9151230a593b1588deec
MilanAkik <mixa.mixony@gmail.com>
2026-03-16 23:59:38 +0100

    Added a second method to the compute shader utilities that calls a shader and gets the 3d texture that the shader generated

--- Assets/Scripts/Utilities/ComputeShaderUtilities.cs
+++ Assets/Scripts/Utilities/ComputeShaderUtilities.cs
@@ -14,5 +14,16 @@ namespace Assets.Scripts.Utilities
             computeShader.Dispatch(kernel, width / 8, height / 8, 1);
             return resultTexture;
         }
+
+        public static RenderTexture ComputeTexture3d(ComputeShader computeShader, string kernelName, int width, int height, int depth, RenderTexture resultTexture)
+        {
+            int kernel = computeShader.FindKernel(kernelName);
+            computeShader.SetInt("width", width);
+            computeShader.SetInt("height", height);
+            computeShader.SetInt("depth", depth);
+            computeShader.SetTexture(kernel, "Result", resultTexture);
+            computeShader.Dispatch(kernel, width / 8, height / 8, depth / 8);
+            return resultTexture;
+        }
     }
 }
