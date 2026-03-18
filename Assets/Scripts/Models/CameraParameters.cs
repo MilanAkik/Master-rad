616db266612096906e9cda2bda90b335947e4989
MilanAkik <mixa.mixony@gmail.com>
2026-03-18 01:18:01 +0100

    Added a model class that represents the camera parameters that are sent to the material and subsequently to the shader

---
+++ Assets/Scripts/Models/CameraParameters.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraParameters
+{
+    public Matrix4x4 CamToWorld { get; set; }
+    public Matrix4x4 CamInverseProjection { get; set; }
+    public Vector3 CamPos { get; set; }
+    public CameraParameters(Matrix4x4 camToWorld, Matrix4x4 camInverseProjection, Vector3 camPos)
+    {
+        CamToWorld = camToWorld;
+        CamInverseProjection = camInverseProjection;
+        CamPos = camPos;
+    }
+}
