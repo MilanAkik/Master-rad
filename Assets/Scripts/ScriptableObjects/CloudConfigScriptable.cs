0f4cb2edb21794198a3d95e991ad81f8aad09c94
MilanAkik <mixa.mixony@gmail.com>
2026-06-10 00:43:05 +0200

    Added a scriptable object for a cloud configuration

---
+++ Assets/Scripts/ScriptableObjects/CloudConfigScriptable.cs
@@ -0,0 +1,8 @@
+using Assets.Scripts.Models;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCloudConfig", menuName = "Config/Spawn config")]
+public class CloudConfigScriptable : ScriptableObject
+{
+    public CloudConfig cloudConfig;   
+}
