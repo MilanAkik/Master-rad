46ae875957c780df9059deb65f2185be24e1d342
MilanAkik <mixa.mixony@gmail.com>
2026-06-10 00:32:25 +0200

    Added a class that is supposed to represent a cloud configuration, containing all the other parameters classes and being serializable

---
+++ Assets/Scripts/Models/CloudConfig.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Models
+{
+    [System.Serializable]
+    public class CloudConfig
+    {
+
+        [SerializeField] private AreaParameters areaParameters;
+        [SerializeField] private CameraParameters cameraParameters;
+        [SerializeField] private CylinderParameters cylinderParameters;
+        [SerializeField] private DensityParameters densityParameters;
+        [SerializeField] private LightParameters lightParameters;
+        [SerializeField] private ShapeParameters shapeParameters;
+
+        public AreaParameters AreaParameters => areaParameters;
+        public CameraParameters CameraParameters => cameraParameters;
+        public CylinderParameters CylinderParameters => cylinderParameters;
+        public DensityParameters DensityParameters => densityParameters;
+        public LightParameters LightParameters => lightParameters;
+        public ShapeParameters ShapeParameters => shapeParameters;
+
+        public CloudConfig(AreaParameters areaParameters, CameraParameters cameraParameters, CylinderParameters cylinderParameters, DensityParameters densityParameters, LightParameters lightParameters, ShapeParameters shapeParameters)
+        {
+            this.areaParameters = areaParameters;
+            this.cameraParameters = cameraParameters;
+            this.cylinderParameters = cylinderParameters;
+            this.densityParameters = densityParameters;
+            this.lightParameters = lightParameters;
+            this.shapeParameters = shapeParameters;
+        }
+
+    }
+}
