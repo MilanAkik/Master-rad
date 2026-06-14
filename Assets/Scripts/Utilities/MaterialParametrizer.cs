c27246011903bd72f0f4165c84e90656225de946
MilanAkik <mixa.mixony@gmail.com>
2026-06-15 00:26:38 +0200

    Calculated all shader parameter ids at the start so that every access doesn't need to do it

--- Assets/Scripts/Utilities/MaterialParametrizer.cs
+++ Assets/Scripts/Utilities/MaterialParametrizer.cs
@@ -3,6 +3,26 @@ using Assets.Scripts.Models;
 
 public static class MaterialParametrizer
 {
+    private static int PropertyId_CamToWorld = Shader.PropertyToID("_CamToWorld");
+    private static int PropertyId_CameraInverseProjection = Shader.PropertyToID("_CamInverseProjection");
+    private static int PropertyId_CamPos = Shader.PropertyToID("_CamPos");
+
+    private static int PropertyId_LightColor = Shader.PropertyToID("_LightColor");
+    private static int PropertyId_LightPosition = Shader.PropertyToID("_LightPosition");
+
+    private static int PropertyId_Cylinders = Shader.PropertyToID("_Cylinders");
+    private static int PropertyId_CylinderCount = Shader.PropertyToID("_CylinderCount");
+
+    private static int PropertyId_RadiusMultiplier = Shader.PropertyToID("_RadiusMultiplier");
+    private static int PropertyId_HeightMultiplier = Shader.PropertyToID("_HeightMultiplier");
+    private static int PropertyId_RadiusThreshold = Shader.PropertyToID("_RadiusThreshold");
+
+    private static int PropertyId_DensityNoise = Shader.PropertyToID("_DensityNoise");
+    private static int PropertyId_DensityNoiseSize = Shader.PropertyToID("_DensityNoiseSize");
+
+    private static int PropertyId_AreaMin = Shader.PropertyToID("_AreaMin");
+    private static int PropertyId_AreaMax = Shader.PropertyToID("_AreaMax");
+
     public static void Parametrize(this Material material, CloudConfig cloudConfig)
     {
         material.Parametrize(cloudConfig.CameraParameters);
@@ -15,39 +35,39 @@ public static class MaterialParametrizer
 
     public static void Parametrize(this Material material, CameraParameters cameraParameters)
     {
-        material.SetMatrix("_CamToWorld", cameraParameters.CamToWorld);
-        material.SetMatrix("_CamInverseProjection", cameraParameters.CamInverseProjection);
-        material.SetVector("_CamPos", cameraParameters.CamPos);
+        material.SetMatrix(PropertyId_CamToWorld, cameraParameters.CamToWorld);
+        material.SetMatrix(PropertyId_CameraInverseProjection, cameraParameters.CamInverseProjection);
+        material.SetVector(PropertyId_CamPos, cameraParameters.CamPos);
     }
 
     public static void Parametrize(this Material material, LightParameters lightParameters)
     {
-        material.SetVector("_LightColor", lightParameters.LightColor);
-        material.SetVector("_LightPosition", lightParameters.LightPosition);
+        material.SetVector(PropertyId_LightColor, lightParameters.LightColor);
+        material.SetVector(PropertyId_LightPosition, lightParameters.LightPosition);
     }
 
     public static void Parametrize(this Material material, CylinderParameters cylinderParameters)
     {
-        material.SetVectorArray("_Cylinders", cylinderParameters.Cylinders);
-        material.SetInt("_CylinderCount", cylinderParameters.CylinderCount);
+        material.SetVectorArray(PropertyId_Cylinders, cylinderParameters.Cylinders);
+        material.SetInt(PropertyId_CylinderCount, cylinderParameters.CylinderCount);
     }
 
     public static void Parametrize(this Material material, ShapeParameters shapeParameters)
     {
-        material.SetFloat("_RadiusMultiplier", shapeParameters.RadiusMultiplier);
-        material.SetFloat("_HeightMultiplier", shapeParameters.HeightMultiplier);
-        material.SetFloat("_RadiusThreshold", shapeParameters.RadiusThreshold);
+        material.SetFloat(PropertyId_RadiusMultiplier, shapeParameters.RadiusMultiplier);
+        material.SetFloat(PropertyId_HeightMultiplier, shapeParameters.HeightMultiplier);
+        material.SetFloat(PropertyId_RadiusThreshold, shapeParameters.RadiusThreshold);
     }
 
     public static void Parametrize(this Material material, DensityParameters densityParameters)
     {
-        material.SetTexture("_DensityNoise", densityParameters.DensityNoise);
-        material.SetInt("_DensityNoiseSize", densityParameters.DensityNoiseSize);
+        material.SetTexture(PropertyId_DensityNoise, densityParameters.DensityNoise);
+        material.SetInt(PropertyId_DensityNoiseSize, densityParameters.DensityNoiseSize);
     }
 
     public static void Parametrize(this Material material, AreaParameters areaParameters)
     {
-        material.SetVector("_areaMin", areaParameters.AreaMin);
-        material.SetVector("_areaMax", areaParameters.AreaMax);
+        material.SetVector(PropertyId_AreaMin, areaParameters.AreaMin);
+        material.SetVector(PropertyId_AreaMax, areaParameters.AreaMax);
     }
 }
