9b5e776fc3d29e3a69528270eac0b0e4c2890e89
MilanAkik <mixa.mixony@gmail.com>
2026-06-14 03:58:33 +0200

    Added the generator and its parameters to the cylinder parameters

--- Assets/Scripts/Models/CylinderParameters.cs
+++ Assets/Scripts/Models/CylinderParameters.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Generators;
 using UnityEngine;
 
 
@@ -6,8 +7,13 @@ namespace Assets.Scripts.Models
     [System.Serializable]
     public class CylinderParameters
     {
+        [SerializeField] private GeneratorParameters generatorParameters;
+        [SerializeField] private CylinderGenerator generator;
         [SerializeField] private Vector4[] cylinders;
         [SerializeField] private int cylinderCount;
+
+        public GeneratorParameters GeneratorParameters => generatorParameters;
+        public CylinderGenerator Generator => generator;
         public Vector4[] Cylinders { get{ return cylinders; } set { cylinders = value; } }
         public int CylinderCount => cylinders.Length;
 
