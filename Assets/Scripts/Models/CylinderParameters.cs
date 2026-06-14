042992f30b0a26638b0ea0b5948e6f9b9128c088
MilanAkik <mixa.mixony@gmail.com>
2026-06-14 04:54:07 +0200

    Moved from representing cyllinder parameters with cylinders to generator and its parameters

--- Assets/Scripts/Models/CylinderParameters.cs
+++ Assets/Scripts/Models/CylinderParameters.cs
@@ -9,18 +9,16 @@ namespace Assets.Scripts.Models
     {
         [SerializeField] private GeneratorParameters generatorParameters;
         [SerializeField] private CylinderGenerator generator;
-        [SerializeField] private Vector4[] cylinders;
-        [SerializeField] private int cylinderCount;
 
         public GeneratorParameters GeneratorParameters => generatorParameters;
         public CylinderGenerator Generator => generator;
-        public Vector4[] Cylinders { get{ return cylinders; } set { cylinders = value; } }
-        public int CylinderCount => cylinders.Length;
+        public Vector4[] Cylinders => generator.getCylinders(generatorParameters);
+        public int CylinderCount => generatorParameters.CylinderCount;
 
-        public CylinderParameters(Vector4[] cylinders, int cylinderCount)
+        public CylinderParameters(GeneratorParameters generatorParameters, CylinderGenerator generator)
         {
-            this.cylinders = cylinders;
-            this.cylinderCount = cylinderCount;
+            this.generatorParameters = generatorParameters;
+            this.generator = generator;
         }
     }
 }
