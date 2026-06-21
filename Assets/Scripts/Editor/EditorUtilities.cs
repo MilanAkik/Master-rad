434501fada7b9d38e32c02ffa8cbca5d639f2797
MilanAkik <mixa.mixony@gmail.com>
2026-06-22 01:29:35 +0200

    Added a method that draws a field at some index relative to some position, binding to some property of some parent with a specific display name

--- Assets/Scripts/Editor/EditorUtilities.cs
+++ Assets/Scripts/Editor/EditorUtilities.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 
 public class EditorUtilities
 {
-    public static Rect GetPropertyPosition(Rect pos, int index)
+
+    public static void DrawField(Rect pos, int index, SerializedProperty parent, string fieldName, string displayName)
     {
-        return new Rect(pos.x, pos.y + index * EditorGUIUtility.singleLineHeight, pos.width, EditorGUIUtility.singleLineHeight);
+        var position = GetPropertyPosition(pos, index, EditorGUIUtility.singleLineHeight, 100f);
+        var field = parent.FindPropertyRelative(fieldName);
+        var content = new GUIContent(displayName);
+        EditorGUI.PropertyField(position, field, content);
+    }
+
+    public static Rect GetPropertyPosition(Rect pos, int index, float fieldHeight, float padding)
+    {
+        return new Rect(pos.x, pos.y + index * (fieldHeight + padding), pos.width, fieldHeight);
     }
 
 }
