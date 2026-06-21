efa7d088de0cf2b10126ba8a48f046bdeaca23a8
MilanAkik <mixa.mixony@gmail.com>
2026-06-21 22:43:00 +0200

    Turned the density texture property from backing property accessor into a calculate and cache, making default value null

--- Assets/Scripts/Models/DensityParameters.cs
+++ Assets/Scripts/Models/DensityParameters.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Utilities;
 using UnityEngine;
 
 namespace Assets.Scripts.Models
@@ -5,10 +6,23 @@ namespace Assets.Scripts.Models
     [System.Serializable]
     public class DensityParameters
     {
-        [SerializeField] private RenderTexture densityNoise;
+        [SerializeField] private RenderTexture densityNoise = null;
         [SerializeField] private int densityNoiseSize;
         [SerializeField] private ComputeShader densityShader;
-        public RenderTexture DensityNoise => densityNoise;
+        public RenderTexture DensityNoise
+        {
+            get
+            {
+                if (densityNoise == null)
+                {
+                    densityNoise = TextureUtilities.CreateRenderTexture3d(densityNoiseSize, densityNoiseSize, densityNoiseSize);
+                    densityNoise = ComputeShaderUtilities.ComputeTexture3d(densityShader, "CSMain", densityNoiseSize, densityNoiseSize, densityNoiseSize, densityNoise);
+                    var tmp = densityNoise.depth;
+                }
+                return densityNoise;
+            }
+        }
+            
         public int DensityNoiseSize => densityNoiseSize;
         public ComputeShader DensityShader => densityShader;
 
