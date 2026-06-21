e2b0f63399548340a9130734ad34e099b552d737
MilanAkik <mixa.mixony@gmail.com>
2026-06-22 00:26:46 +0200

    Added a basic drawer for the shape parameters that draws just one of the floats just as a trial The file will be renamed later

---
+++ Assets/Scripts/Editor/CloudConfigDrawer.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Models;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    [CustomPropertyDrawer(typeof(ShapeParameters))]
+    public class CloudConfigDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect pos, SerializedProperty prop, GUIContent label)
+        {
+            EditorGUI.BeginProperty(pos, label, prop);
+            EditorGUI.PropertyField( pos, prop.FindPropertyRelative("radiusMultiplier"), new GUIContent("Radius Multiplier"));
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty prop, GUIContent label)
+        {
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+
+}
