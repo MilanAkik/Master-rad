241e6a8c7251e34bd06218e2fcef4070e9ab8208
MilanAkik <mixa.mixony@gmail.com>
2026-07-26 01:29:24 +0200

    Moved the ranges to the private fields and referenced it in the generation Also added the height multiplier back

--- Assets/Scripts/Generators/RandomCylinderGenerator.cs
+++ Assets/Scripts/Generators/RandomCylinderGenerator.cs
@@ -9,6 +9,12 @@ namespace Assets.Scripts.Generators
     public class RandomCylinderGenerator : CylinderGenerator
     {
 
+        private Vector2 _xRange = new Vector2(-1.0f, 1.0f);
+        private Vector2 _yRange = new Vector2(-0.1f, 0.1f);
+        private Vector2 _zRange = new Vector2(-1.0f, 1.0f);
+        private Vector2 _rRange = new Vector2(0.01f, 0.99f);
+        private Vector2 _hRange = new Vector2(0.01f, 0.99f);
+
         public override Vector4[] getCylinders(GeneratorParameters parameters)
         {
             int count = parameters.CylinderCount;
@@ -37,13 +43,13 @@ namespace Assets.Scripts.Generators
             Random.InitState(seed);
             for (int i = 0; i < count; i++)
             {
-                var x = Random.Range(-1.0f, 1.0f);
-                var y = Random.Range(-0.1f, 0.1f);
-                var z = Random.Range(-1.0f, 1.0f);
-                var r = Random.Range(0.01f, 0.99f);
-                var h = Random.Range(0.01f, 0.99f);
+                var x = Random.Range(_xRange.x, _xRange.y);
+                var y = Random.Range(_yRange.x, _yRange.y);
+                var z = Random.Range(_zRange.x, _zRange.y) + 5.0f;
+                var r = Random.Range(_rRange.x, _rRange.y) * parameters.RadiusMultiplier;
+                var h = Random.Range(_hRange.x, _hRange.y) * parameters.HeightMultiplier;
                 res[i] = new Matrix4x4(
-                    new Vector4(x, y, z + 5.0f, r * parameters.RadiusMultiplier),
+                    new Vector4(x, y, z, r),
                     new Vector4(h,0,0,0),
                     Vector4.zero,
                     Vector4.zero
@@ -52,6 +58,6 @@ namespace Assets.Scripts.Generators
             Random.state = prevState;
             return res;
         }
-
+        
     }
 }
