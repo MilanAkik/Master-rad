85e87ffedb2fe2fa12fe95a789e23d3442da5285
MilanAkik <mixa.mixony@gmail.com>
2026-07-26 22:24:50 +0200

    Removed the method for generating the cylinders vectors and left only the cylinders matrix generation in all the generator classes

--- Assets/Scripts/Generators/ICylinderGenerator.cs
+++ Assets/Scripts/Generators/ICylinderGenerator.cs
@@ -5,7 +5,6 @@ namespace Assets.Scripts.Generators
 {
     internal interface ICylinderGenerator
     {
-        public abstract Vector4[] getCylinders(GeneratorParameters parameters);
         public abstract Matrix4x4[] getCylinderMatrices(GeneratorParameters parameters);
     }
 }
