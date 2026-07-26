65636a88704bcefdbc2e341e2997b7f8f50affab
MilanAkik <mixa.mixony@gmail.com>
2026-07-26 02:40:05 +0200

    Pulled out the texture update into a private method

--- Assets/Scripts/Generators/NoiseCylinderGenerator.cs
+++ Assets/Scripts/Generators/NoiseCylinderGenerator.cs
@@ -99,21 +99,7 @@ namespace Assets.Scripts.Generators
                 float z = ((float)maxj / (float)textureSize) * 2f - 1f;
                 float r = tex.GetPixel((maxi + _rOffset.x) % textureSize, (maxj + _rOffset.y) % textureSize).r;
                 float h = tex.GetPixel((maxi + _hOffset.x) % textureSize, (maxj + _hOffset.y) % textureSize).r;
-                int radius = 8;
-                for (int i = -radius; i < radius + 1; i++)
-                {
-                    for (int j = -radius; j < radius + 1; j++)
-                    {
-                        if (i * i + j * j < radius * radius)
-                        {
-                            var dist = Mathf.Sqrt(i * i + j * j);
-                            var oldCol = tex.GetPixel(maxi + i, maxj + j);
-                            var oldVal = oldCol.r;
-                            var newVal = oldVal * 0.8f * (dist / (float)radius);
-                            tex.SetPixel(maxi + i, maxj + j, new Color(newVal, newVal, newVal, 1.0f));
-                        }
-                    }
-                }
+                UpdateTexture(tex, maxi, maxj);
                 if (x == 0) x = 0.001f;
                 if (y == 0) y = 0.001f;
                 if (z == 0) z = 0.001f;
@@ -150,5 +136,22 @@ namespace Assets.Scripts.Generators
             return (maxi, maxj);
         }
 
+        private void UpdateTexture(Texture2D tex, int maxi, int maxj, int radius = 8)
+        {
+            for (int i = -radius; i < radius + 1; i++)
+            {
+                for (int j = -radius; j < radius + 1; j++)
+                {
+                    if (i * i + j * j < radius * radius)
+                    {
+                        var dist = Mathf.Sqrt(i * i + j * j);
+                        var oldCol = tex.GetPixel(maxi + i, maxj + j);
+                        var oldVal = oldCol.r;
+                        var newVal = oldVal * 0.8f * (dist / (float)radius);
+                        tex.SetPixel(maxi + i, maxj + j, new Color(newVal, newVal, newVal, 1.0f));
+                    }
+                }
+            }
+        }
     }
 }
