33b3faf15e90a293c0cfa37bb6afcf71db1056cf
MilanAkik <mixa.mixony@gmail.com>
2026-07-27 23:14:14 +0200

    Added area min and area max to the generator parameters

--- Assets/Scripts/Models/GeneratorParameters.cs
+++ Assets/Scripts/Models/GeneratorParameters.cs
@@ -9,10 +9,14 @@ namespace Assets.Scripts.Models
         [SerializeField] private int randomSeed;
         [SerializeField] private float radiusMultiplier;
         [SerializeField] private float heightMultiplier;
+        [SerializeField] private Vector3 areaMin;
+        [SerializeField] private Vector3 areaMax;
 
         public int CylinderCount { get => cylinderCount; set => cylinderCount = value; }
         public int RandomSeed { get => randomSeed; set => randomSeed = value; }
         public float RadiusMultiplier { get => radiusMultiplier; set => radiusMultiplier = value; }
         public float HeightMultiplier { get => heightMultiplier; set => heightMultiplier = value; }
+        public Vector3 AreaMin { get => areaMin; set => areaMin = value; }
+        public Vector3 AreaMax { get => areaMax; set => areaMax = value; }
     }
 }
