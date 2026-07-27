87a9870ac6d7ba18f21ab30790cf9ab3d0c6c462
MilanAkik <mixa.mixony@gmail.com>
2026-07-28 00:11:28 +0200

    Removed the now unused area parameters from the cloud config

--- Assets/Scripts/Models/CloudConfig.cs
+++ Assets/Scripts/Models/CloudConfig.cs
@@ -6,21 +6,18 @@ namespace Assets.Scripts.Models
     public class CloudConfig
     {
 
-        [SerializeField] private AreaParameters areaParameters;
         [SerializeField] private CameraParameters cameraParameters;
         [SerializeField] private CylinderParameters cylinderParameters;
         [SerializeField] private DensityParameters densityParameters;
         [SerializeField] private LightParameters lightParameters;
 
-        public AreaParameters AreaParameters => areaParameters;
         public CameraParameters CameraParameters { get => cameraParameters; set => cameraParameters = value; }
         public CylinderParameters CylinderParameters => cylinderParameters;
         public DensityParameters DensityParameters => densityParameters;
         public LightParameters LightParameters => lightParameters;
 
-        public CloudConfig(AreaParameters areaParameters, CameraParameters cameraParameters, CylinderParameters cylinderParameters, DensityParameters densityParameters, LightParameters lightParameters)
+        public CloudConfig(CameraParameters cameraParameters, CylinderParameters cylinderParameters, DensityParameters densityParameters, LightParameters lightParameters)
         {
-            this.areaParameters = areaParameters;
             this.cameraParameters = cameraParameters;
             this.cylinderParameters = cylinderParameters;
             this.densityParameters = densityParameters;
