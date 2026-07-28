1a329cd3ff6f92716dafc3b080b96f7c5d3087e4
MilanAkik <mixa.mixony@gmail.com>
2026-07-29 01:02:27 +0200

    Added separate private dummy methods for each enum type and a switch case calling each for their type

--- Assets/Scripts/Generators/ShapesCylinderGenerator.cs
+++ Assets/Scripts/Generators/ShapesCylinderGenerator.cs
@@ -12,6 +12,17 @@ namespace Assets.Scripts.Generators
 
         public ShapeType shapeType = ShapeType.Cube;
 
-        public override Matrix4x4[] getCylinderMatrices(GeneratorParameters parameters) => new Matrix4x4[parameters.CylinderCount];
+        public override Matrix4x4[] getCylinderMatrices(GeneratorParameters parameters) => shapeType switch
+        {
+            ShapeType.Cube => GenerateCubeCylinders(parameters),
+            ShapeType.Circle => GenerateCircleCylinders(parameters),
+            ShapeType.Sphere => GenerateSphereCylinders(parameters),
+            _ => new Matrix4x4[parameters.CylinderCount]
+        };
+
+        private Matrix4x4[] GenerateCubeCylinders(GeneratorParameters parameters) => new Matrix4x4[parameters.CylinderCount];
+        private Matrix4x4[] GenerateCircleCylinders(GeneratorParameters parameters) => new Matrix4x4[parameters.CylinderCount];
+        private Matrix4x4[] GenerateSphereCylinders(GeneratorParameters parameters) => new Matrix4x4[parameters.CylinderCount];
+
     }
 }
