2f1dc3d4d43b2035ba02967a2ae8f6e32932fd33
MilanAkik <mixa.mixony@gmail.com>
2026-08-02 20:53:13 +0200

    Added the axioms and the starting state to the l system generator

--- Assets/Scripts/Generators/LSystemGenerator.cs
+++ Assets/Scripts/Generators/LSystemGenerator.cs
@@ -6,6 +6,19 @@ namespace Assets.Scripts.Generators
     [CreateAssetMenu(menuName = "CylinderGenerator/LSystem")]
     public class LSystemGenerator : CylinderGenerator
     {
+        // Foreward, Backward, Left, Right, Up, Down, Grow, Shrink, Place, VSplit
+        public string axiomF = "";
+        public string axiomB = "";
+        public string axiomL = "";
+        public string axiomR = "";
+        public string axiomU = "";
+        public string axiomD = "";
+        public string axiomG = "";
+        public string axiomS = "";
+        public string axiomP = "";
+        public string axiomV = "";
+        public string startingState = "";
+
         public override Matrix4x4[] getCylinderMatrices(GeneratorParameters parameters) => new Matrix4x4[parameters.CylinderCount];
     }
 }
