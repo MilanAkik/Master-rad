cd705dc041e972e8a0bb4e5778fbbcf0856ffc9e
MilanAkik <mixa.mixony@gmail.com>
2026-08-22 00:35:44 +0200

    Made density texture not serializable, added the checks besides the nullability to check if texture needs to be regenerated, and added the release mechanism for the texture

--- Assets/Scripts/Models/DensityParameters.cs
+++ Assets/Scripts/Models/DensityParameters.cs
@@ -6,21 +6,30 @@ namespace Assets.Scripts.Models
     [System.Serializable]
     public class DensityParameters
     {
-        [SerializeField] private RenderTexture densityNoise = null;
+        [System.NonSerialized] private RenderTexture densityNoise = null;
         [SerializeField] private int densityNoiseSize;
         [SerializeField] private ComputeShader densityShader;
         public RenderTexture DensityNoise
         {
             get
             {
-                if (densityNoise == null)
+                if (densityNoise == null || !densityNoise.IsCreated() || densityNoise.width != densityNoiseSize || densityNoise.height != densityNoiseSize || densityNoise.volumeDepth != densityNoiseSize)
                 {
+                    ReleaseDensityNoise();
                     densityNoise = TextureUtilities.CreateRenderTexture3d(densityNoiseSize, densityNoiseSize, densityNoiseSize);
                     densityNoise = ComputeShaderUtilities.ComputeTexture3d(densityShader, "CSMain", densityNoiseSize, densityNoiseSize, densityNoiseSize, densityNoise);
-                    var tmp = densityNoise.depth;
                 }
                 return densityNoise;
             }
+        }
+
+        private void ReleaseDensityNoise()
+        {
+            if (densityNoise == null) return;
+            densityNoise.Release();
+            if (Application.isPlaying) Object.Destroy(densityNoise);
+            else Object.DestroyImmediate(densityNoise);
+            densityNoise = null;
         }
             
         public int DensityNoiseSize => densityNoiseSize;
