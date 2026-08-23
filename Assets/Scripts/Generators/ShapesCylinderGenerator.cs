f7ed96aeaa5dc3399b60c44fe3242930caf8f280
MilanAkik <mixa.mixony@gmail.com>
2026-08-23 15:43:19 +0200

    Fixed the bug in the cube generation code and refactored it a bit

--- Assets/Scripts/Generators/ShapesCylinderGenerator.cs
+++ Assets/Scripts/Generators/ShapesCylinderGenerator.cs
@@ -25,10 +25,16 @@ namespace Assets.Scripts.Generators
         private Matrix4x4[] GenerateCubeCylinders(GeneratorParameters parameters)
         {
             var count = parameters.CylinderCount;
+            var areaMin = parameters.AreaMin;
+            var areaMax = parameters.AreaMax;
             var matrices = new Matrix4x4[count];
             int i = 0;
             for (; i * i * i < count; i++);
             float size = (float)i;
+            var xSize = areaMax.x - areaMin.x;
+            var zSize = areaMax.z - areaMin.z;
+            var radMod = (xSize < zSize ? xSize : zSize);
+            var hMod = areaMax.y - areaMin.y;
             for (int x = 0; x < size; x++)
             {
                 for(int y = 0; y < size; y++)
@@ -38,11 +44,11 @@ namespace Assets.Scripts.Generators
                         int index = (int)(x * size * size + y * size + z);
                         if (index < parameters.CylinderCount)
                         {
-                            float posX = Mathf.Lerp(parameters.AreaMin.x, parameters.AreaMax.x, x / (size - 1));
-                            float posY = Mathf.Lerp(parameters.AreaMin.y, parameters.AreaMax.y, y / (size - 1));
-                            float posZ = Mathf.Lerp(parameters.AreaMin.z, parameters.AreaMax.z, z / (size - 1));
-                            float h = (1 / size) * parameters.HeightMultiplier;
-                            float r = (1 / size) * parameters.RadiusMultiplier;
+                            float posX = Mathf.Lerp(areaMin.x, areaMax.x, (x+0.5f) / size);
+                            float posY = Mathf.Lerp(areaMin.y, areaMax.y, y / size);
+                            float posZ = Mathf.Lerp(areaMin.z, areaMax.z, (z+0.5f) / size);
+                            float h = (1f / size) * hMod * parameters.HeightMultiplier;
+                            float r = (0.5f / size) * radMod * parameters.RadiusMultiplier;
                             matrices[index] = new Matrix4x4(new Vector4(posX, posY, posZ, r), new Vector4(h, 0, 0, 0), Vector4.zero, Vector4.zero);
                         }
                     }
